87347d8f590a50febc5d755f9942d56705a9db7a
BIROOM <zoon1031@naver.com>
2022-04-22 12:43:55 +0900

    package-update

---
+++ .postcssrc.js
@@ -0,0 +1,11 @@
+//Node js 방식이므로 Common JS방식을 사용
+//import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer');
+
+//export
+module.exports = {
+    plugins: [
+        // autoprefixer
+        require('autoprefixer')
+    ]
+}
